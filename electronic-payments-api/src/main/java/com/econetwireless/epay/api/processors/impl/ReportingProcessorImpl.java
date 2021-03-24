8f538da8c6252ca7fe801607332b504511a0183c
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-03-24 23:39:28 +0200

    Added some logging to debug code

--- electronic-payments-api/src/main/java/com/econetwireless/epay/api/processors/impl/ReportingProcessorImpl.java
+++ electronic-payments-api/src/main/java/com/econetwireless/epay/api/processors/impl/ReportingProcessorImpl.java
@@ -3,8 +3,11 @@ package com.econetwireless.epay.api.processors.impl;
 import com.econetwireless.epay.api.processors.api.ReportingProcessor;
 import com.econetwireless.epay.api.rest.messages.TransactionsResponse;
 import com.econetwireless.epay.business.services.api.ReportingService;
+import com.econetwireless.epay.business.services.impl.CreditsServiceImpl;
 import com.econetwireless.epay.domain.SubscriberRequest;
 import com.econetwireless.utils.enums.ResponseCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -15,14 +18,19 @@ public class ReportingProcessorImpl implements ReportingProcessor{
 
     private ReportingService reportingService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreditsServiceImpl.class);
+
     public ReportingProcessorImpl(ReportingService reportingService) {
         this.reportingService = reportingService;
     }
 
     @Override
     public TransactionsResponse getPartnerTransactions(final String partnerCode) {
+        LOGGER.info(">>>>>>>>>>>>>>>>>>.NOW IN GET PARTNER TRANSACTION {}: ",partnerCode);
         final TransactionsResponse transactionsResponse = new TransactionsResponse();
+        LOGGER.info(">>>>>>>>>>>>>>>>>>.NOW IN GET PARTNER TRANSACTION {}: ",partnerCode);
         final List<SubscriberRequest> subscriberRequests = reportingService.findSubscriberRequestsByPartnerCode(partnerCode);
+        LOGGER.info(">>>>>>>>>>>>>>>The returned subscribers by code {} ",subscriberRequests.toString());
         transactionsResponse.setResponseCode(ResponseCode.SUCCESS.getCode());
         transactionsResponse.setNarrative("Successful search");
         transactionsResponse.setSubscriberRequests(subscriberRequests);
