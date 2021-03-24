737360c94ae77c8a7c5bdf3b29d4c1b16940e6fb
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-03-24 23:28:50 +0200

    Created a new BusinesApiTests File

---
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/api/rest/BusinessApiTests.java
@@ -0,0 +1,11 @@
+package com.econetwireless.epay.api.rest;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+
+public class BusinessApiTests {
+
+}
