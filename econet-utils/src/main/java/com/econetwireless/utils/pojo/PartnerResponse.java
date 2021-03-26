d8fd560c6bf2eb9deb21a8bf01bd2198bdec6a12
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-03-26 15:53:59 +0200

    Created a PartnerResponseDTO

---
+++ econet-utils/src/main/java/com/econetwireless/utils/pojo/PartnerResponse.java
@@ -0,0 +1,40 @@
+package com.econetwireless.utils.pojo;
+
+public class PartnerResponse {
+    private String code;
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "PartnerResponse{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
