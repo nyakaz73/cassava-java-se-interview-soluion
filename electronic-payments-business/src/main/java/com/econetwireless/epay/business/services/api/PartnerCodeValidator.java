a732726393f4817514b9f959036ecf2ad5f00312
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-03-26 16:28:57 +0200

    Added validatePartnerCodeData

--- electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/api/PartnerCodeValidator.java
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/api/PartnerCodeValidator.java
@@ -1,9 +1,13 @@
 package com.econetwireless.epay.business.services.api;
 
+import com.econetwireless.utils.pojo.PartnerResponse;
+
 /**
  * Created by tnyamakura on 18/3/2017.
  */
 public interface PartnerCodeValidator {
 
     public boolean validatePartnerCode(String partnerCode);
+
+    public PartnerResponse validatePartnerCodeData(String partnerCode);
 }
--- electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImpl.java
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImpl.java
@@ -1,14 +1,21 @@
 package com.econetwireless.epay.business.services.impl;
 
+import com.econetwireless.epay.business.rest.resources.EpayBusinessResource;
 import com.econetwireless.epay.business.services.api.PartnerCodeValidator;
 import com.econetwireless.epay.dao.requestpartner.api.RequestPartnerDao;
+import com.econetwireless.epay.domain.RequestPartner;
 import com.econetwireless.utils.enums.ResponseCode;
 import com.econetwireless.utils.execeptions.EpayException;
+import com.econetwireless.utils.pojo.PartnerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by tnyamakura on 18/3/2017.
  */
 public class PartnerCodeValidatorImpl implements PartnerCodeValidator{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PartnerCodeValidator.class);
 
     private RequestPartnerDao requestPartnerDao;
 
@@ -24,5 +31,20 @@ public class PartnerCodeValidatorImpl implements PartnerCodeValidator{
             throw new EpayException(ResponseCode.INVALID_REQUEST, "Invalid partner code supplied : "+partnerCode);
         }
         return isValidPartner;
+    }
+
+    @Override
+    public PartnerResponse validatePartnerCodeData(String partnerCode) {
+
+        RequestPartner requestPartner = requestPartnerDao.findByCode(partnerCode);
+        if(requestPartner == null) {
+            throw new EpayException(ResponseCode.INVALID_REQUEST, "Invalid partner code supplied : "+partnerCode);
+        }
+        PartnerResponse response = new PartnerResponse();
+        response.setCode(requestPartner.getCode());
+        response.setDescription(requestPartner.getDescription());
+        response.setName(requestPartner.getName());
+        LOGGER.info(response.toString());
+        return response;
     }
 }
