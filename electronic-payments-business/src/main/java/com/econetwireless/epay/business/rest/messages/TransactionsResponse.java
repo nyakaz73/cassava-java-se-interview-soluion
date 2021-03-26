dbece25c244406b368166cce7639898e212bfdf4
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-03-26 16:25:46 +0200

    Created TransactionResponse

---
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/rest/messages/TransactionsResponse.java
@@ -0,0 +1,45 @@
+package com.econetwireless.epay.business.rest.messages;
+
+import com.econetwireless.epay.domain.SubscriberRequest;
+
+import java.util.List;
+
+public class TransactionsResponse {
+    private String responseCode;
+    private String narrative;
+    private String partnerCode;
+    private List<SubscriberRequest> subscriberRequests;
+
+
+    public String getPartnerCode() {
+        return partnerCode;
+    }
+
+    public void setPartnerCode(String partnerCode) {
+        this.partnerCode = partnerCode;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getNarrative() {
+        return narrative;
+    }
+
+    public void setNarrative(String narrative) {
+        this.narrative = narrative;
+    }
+
+    public List<SubscriberRequest> getSubscriberRequests() {
+        return subscriberRequests;
+    }
+
+    public void setSubscriberRequests(List<SubscriberRequest> subscriberRequests) {
+        this.subscriberRequests = subscriberRequests;
+    }
+}
